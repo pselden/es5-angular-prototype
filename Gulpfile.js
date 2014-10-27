85a4b70156012addb73e2b66ec266331b9638fa0
Paul Selden <pselden@axs.com>
2014-10-27 14:48:11 -0400

    Try out minifyify with external sourcemap.

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,26 +4,22 @@ var browserify = require('browserify');
 var watchify = require('watchify');
 
 var changed = require('gulp-changed');
-var uglify = require('gulp-uglify');
 var streamify = require('gulp-streamify'); // required for uglify because it has no streaming mode
 var es6ify = require('es6ify');
 es6ify.traceurOverrides = { blockBinding: true }; // allow "let"
 
-// gulp helpers
-var gulpif = require('gulp-if');
 var gutil = require('gulp-util');
 var size = require('gulp-size');
 var source = require('vinyl-source-stream');
-
 var staticFiles = ['./eventshopper/**/*.html', './eventshopper/**/*.css'];
 
-function bundle(shouldWatch, options){
+function bundle(options){
     watchify.args.debug = true; // note: this generates an inline sourcemap -- not a sourcemap file
     var bundler = browserify(es6ify.runtime, watchify.args)
         .transform(es6ify)
         .add('./eventshopper/app.js');
 
-    if(shouldWatch){
+    if(options.watch){
         bundler = watchify(bundler);
 
         gulp.watch(staticFiles, ['copy']);
@@ -31,12 +27,15 @@ function bundle(shouldWatch, options){
         bundler.on('update', rebundle);
     }
 
+    if(options.minify) {
+        bundler.plugin('minifyify', {map: '/app.map.json', output: './dist/app.map.json' } )
+    }
+
     function rebundle() {
         return bundler
             .bundle()
             .on('error', gutil.log.bind(gutil, 'Browserify Error'))
             .pipe(source('app.js'))
-            .pipe(gulpif(options.minify, streamify(uglify())))
 	        .pipe(streamify(size()))
             .pipe(gulp.dest('dist'));
     }
@@ -51,11 +50,11 @@ gulp.task('copy', function(){
 });
 
 gulp.task('watch', function() {
-    return bundle(true, { minify: false });
+    return bundle({ watch: true, minify: false });
 });
 
 gulp.task('bundle', function(){
-    return bundle(false, { minify: true });
+    return bundle({ watch: false, minify: true });
 });
 
 gulp.task('default', ['copy', 'watch']);
